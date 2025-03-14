6b3ea73a043da27660f06adb50beba6814b2c983
Elena Polukova <e.brykova@yofin.ru>
2025-03-14 11:55:32 +0400

    Fixed

--- stats/stats-client/src/main/java/ru/practicum/ewm/client/StatClient.java
+++ stats/stats-client/src/main/java/ru/practicum/ewm/client/StatClient.java
@@ -1,56 +1,24 @@
 package ru.practicum.ewm.client;
 
-import jakarta.validation.Valid;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.MediaType;
-import org.springframework.stereotype.Component;
-import org.springframework.web.client.ResourceAccessException;
-import org.springframework.web.client.RestClient;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import ru.practicum.dto.HitDto;
 import ru.practicum.dto.StatDto;
 
 import java.util.List;
 
-@Component
-@Slf4j
-public class StatClient {
-    private final RestClient restClient;
+@FeignClient(name = "stats-server")
+public interface StatClient {
 
-    StatClient(@Value("${stats-server.url}") String serverUrl) {
-        restClient = RestClient.create(serverUrl);
-    }
+    @PostMapping("/hit")
+    void saveHit(@RequestBody HitDto hitDto);
 
-    public void hit(@Valid HitDto hitDto) {
-        try {
-            restClient.post().uri("/hit")
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .body(hitDto)
-                    .retrieve()
-                    .toBodilessEntity();
-        } catch (Exception exception) {
-            log.error("При обращении к сервису статистики возникла ошибка: " + exception.getMessage());
-        }
-    }
-
-    public List<StatDto> getStats(String start,
-                                  String end,
-                                  List<String> uris,
-                                  Boolean unique) {
-        try {
-            return restClient.get()
-                    .uri(uriBuilder -> uriBuilder
-                            .path("/stats")
-                            .queryParam("start", start)
-                            .queryParam("end", end)
-                            .queryParam("uris", uris)
-                            .queryParam("unique", unique)
-                            .build())
-                    .retrieve().body(new ParameterizedTypeReference<>() {
-                    });
-        } catch (ResourceAccessException ignored) {
-        }
-        return List.of();
-    }
+    @GetMapping("stats")
+    List<StatDto> getStats(@RequestParam(defaultValue = "") String start,
+                           @RequestParam(defaultValue = "") String end,
+                           @RequestParam(defaultValue = "") List<String> uris,
+                           @RequestParam(defaultValue = "false") Boolean unique);
 }
